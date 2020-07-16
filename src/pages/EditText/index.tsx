9b3f361d68f62bc5d7df822ad9e400acfeccd669
Suriel <mhecy98@gmail.com>
2020-07-16 11:30:15 +0800

    fix:升级react-navigation

---
+++ src/pages/EditText/index.tsx
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import { Alert, View, TextInput, StyleSheet } from 'react-native';
+import StatusBar from '@components/StatusBar';
+const EditTextScreen = ({ navigation }: any) => {
+    const [text, setText] = React.useState('');
+
+    const hasUnsavedChanges = Boolean(text);
+
+
+
+    React.useEffect(() => {
+        // console.log('useEffect')
+
+        // navigation.addListener('beforeRemove', (e: any) => {
+        //     console.log('e', e)
+        //     const action = e.data.action;
+        //     if (!hasUnsavedChanges) {
+        //         console.log('!hasUnsavedChanges', !hasUnsavedChanges)
+
+        //         return;
+        //     }
+
+        //     console.log('!preventDefault', e.preventDefault)
+
+        //     e.preventDefault();
+
+        //     Alert.alert(
+        //         'Discard changes?',
+        //         'You have unsaved changes. Are you sure to discard them and leave the screen?',
+        //         [
+        //             { text: "Don't leave", style: 'cancel', onPress: () => { } },
+        //             {
+        //                 text: 'Discard',
+        //                 style: 'destructive',
+        //                 onPress: () => navigation.dispatch(action),
+        //             },
+        //         ]
+        //     );
+        //     console.log('!Alert')
+        // }),
+        navigation.addListener('beforeRemove', (e: any) => {
+            console.log('111')
+            const action = e.data.action;
+            if (!hasUnsavedChanges) {
+                console.log('!hasUnsavedChanges', !hasUnsavedChanges)
+
+                return;
+            }
+            e.preventDefault();
+
+            Alert.alert(
+                'Discard changes?',
+                'You have unsaved changes. Are you sure to discard them and leave the screen?',
+                [
+                    { text: "Don't leave", style: 'cancel', onPress: () => { } },
+                    {
+                        text: 'Discard',
+                        style: 'destructive',
+                        onPress: () => navigation.dispatch(action),
+                    },
+                ]
+            );
+        })
+    }, [hasUnsavedChanges, navigation])
+
+    return (
+        <View style={styles.content}>
+            <StatusBar translucent={true} barStyle="dark-content" backgroundColor="#fff" />
+            <TextInput
+                autoFocus
+                style={styles.input}
+                value={text}
+                placeholder="Type something…"
+                onChangeText={setText}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        padding: 16,
+    },
+    input: {
+        margin: 8,
+        padding: 10,
+        borderRadius: 3,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: 'rgba(0, 0, 0, 0.08)',
+        backgroundColor: 'white',
+    },
+    buttons: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 8,
+    },
+    button: {
+        margin: 8,
+    },
+});
+
+
+export default EditTextScreen
